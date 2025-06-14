a73127f81ca59025bc7ca4dd300edb854c6dcf7b
AkshatGupta2005 <akshatguptaip@gmail.com>
2025-06-14 21:17:22 +0530

    API link final

--- server/server.js
+++ server/server.js
@@ -316,6 +316,17 @@ app.get("/api/industries", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+app.get("/api/medicalinstitute", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM medicalinstitute ORDER BY id ASC"
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Database fetch error:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
